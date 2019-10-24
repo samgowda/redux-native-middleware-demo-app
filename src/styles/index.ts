81a000b0ac6d843362cbd07d21d49988a429da2f
Sam Gowda <samgowda@gmail.com>
2019-10-24 17:03:34 -0400

    inject global styles

---
+++ src/styles/index.ts
@@ -0,0 +1,14 @@
+import styledNormalize from "styled-normalize";
+import { injectGlobal } from "styled-components";
+
+export default () => injectGlobal`
+  ${styledNormalize}
+
+  body {
+    font-family: arial, sans-serif;
+  }
+
+  textarea:focus, input:focus, button: focus {
+    outline: none;
+  }
+`;
